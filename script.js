255a9b201debf21fadc29a3bc6ddb3b776234f37
frederic <dewaele-frederic@outlook.fr>
2021-07-15 17:35:45 +0200

    maj html/css

--- script.js
+++ script.js
@@ -1,77 +1,82 @@
-var player, score, scoreTotal, gaming;
+var player = 1;
+var score = 0;
+var scoreTotal = 0;
+var gaming = true;
 
-init ()
+
+reset ();
 
                            //fonction résultat
 
 function result () {
-if(gaming) {
+if(gaming = true) {
 
 var number = Math.floor(Math.random() * 6) + 1;
 var x = document.querySelector('.dice');
 x.src = 'images/dice-' + number + '.png';
 
 if(number !== 1) {
-
+  
 score += number;
-document.querySelector('#scoreCurrent' + player).textContent = score;
-}
+document.querySelector('.scoreCurrent' + player).textContent = score;
+console.log(number)
 
+}
 else {
 
   nextPlayer ();
 }
 }};
 
-
                        //fonction joueur suivant
+
 function nextPlayer () {
 
 player === 1 ? player = 2 : player = 1;
 score = 0;
 
-document.querySelector('#scoreCurrent1').textContent = '0';
-document.querySelector('#scoreCurrent2').textContent ='0';
+document.querySelector('.scoreCurrent1').textContent = '0';
+document.querySelector('.scoreCurrent2').textContent = '0';
 
 document.querySelector('.player1').classList.toggle('turn');
 document.querySelector('.player2').classList.toggle('turn');
+};
 
-}
 
-                            document.querySelector('.btn-roll').addEventListener('click', result)
+document.querySelector('.btn-roll').addEventListener('click', result)
 
                            //fonction hold
+
 function hold () {
-if(gaming) {
+if(gaming = true) {
 
 scoreTotal += score;
-document.querySelector('#score' + player).textContent = scoreTotal;
-}
-
-if(scoreTotal >= 100) {
-gaming = false;
+document.querySelector('.score' + player).textContent = scoreTotal;
+console.log(scoreTotal)
+nextPlayer(); 
 
 }
 else {
+  gaming = false;
+}};
 
-nextPlayer() 
-}}
 
+document.querySelector('.btn-hold').addEventListener('click', hold)
 
-                            document.querySelector('.btn-hold').addEventListener('click', hold)
-
-function init () {
+                          //function reset
+function reset () {
 
   player = 1;
   gaming = true;
-  
+  score = 0;
+  scoreTotal = 0;
 
+  document.querySelector('.scoreCurrent1').textContent = '0';
+  document.querySelector('.scoreCurrent2').textContent = '0';
+  document.querySelector('.score1').textContent = '0';
+  document.querySelector('.score2').textContent = '0';
+  document.querySelector('.player1').classList.add('turn');
+  document.querySelector('.player2').classList.remove('turn');
+};
 
-  document.getElementById('scoreCurrent1').textContent = '0';
-  document.getElementById('scoreCurrent2').textContent = '0';
-  document.getElementById('score1').textContent = '0';
-  document.getElementById('score2').textContent = '0';
-}
-
-
-                            document.querySelector('.btn-game').addEventListener('click', init)
+document.querySelector('.btn-game').addEventListener('click', reset)
