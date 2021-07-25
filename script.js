7750cd4a2267e4b395959f0e5068f138d501c9db
frederic <dewaele-frederic@outlook.fr>
2021-07-25 10:32:41 +0200

    maj js

--- script.js
+++ script.js
@@ -1,82 +1,100 @@
-var player = 1;
-var score = 0;
-var scoreTotal = 0;
-var gaming = true;
+var player1 = document.querySelector('.player1');
+var name1 = document.querySelector('.name1');
+var scoreTotal1 = document.querySelector('.score1');
+var score1 = document.querySelector('.scoreCurrent1');
 
+var player2 = document.querySelector('.player2');
+var name2 = document.querySelector('.name2');
+var scoreTotal2 = document.querySelector('.score2');
+var score2 = document.querySelector('.scoreCurrent2');
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+
+var activePlayer = player1;
 
 reset ();
 
-                           //fonction résultat
+                      //function roll
 
-function result () {
-if(gaming = true) {
+function roll () {
 
 var number = Math.floor(Math.random() * 6) + 1;
 var x = document.querySelector('.dice');
 x.src = 'images/dice-' + number + '.png';
 
-if(number !== 1) {
-  
-score += number;
-document.querySelector('.scoreCurrent' + player).textContent = score;
-console.log(number)
-
-}
-else {
-
-  nextPlayer ();
-}
-}};
+if(number == 1){
+  if(activePlayer == player1){
+    score1.textContent = '0';
+  }
+  else{
+    score2.textContent = '0';
+  }
+  nextPlayer();}
+  else{
+    if(activePlayer == player1){
+      score1.textContent = parseInt(score1.textContent) + number;
+    }
+    else{
+      score2.textContent = parseInt(score2.textContent) + number;
+    }
+  }};
 
                        //fonction joueur suivant
 
 function nextPlayer () {
 
-player === 1 ? player = 2 : player = 1;
-score = 0;
+activePlayer = activePlayer == player1 ? player2 : player1;
 
-document.querySelector('.scoreCurrent1').textContent = '0';
-document.querySelector('.scoreCurrent2').textContent = '0';
-
+score1.textContent = '0';
+score2.textContent = '0';
 document.querySelector('.player1').classList.toggle('turn');
 document.querySelector('.player2').classList.toggle('turn');
 };
 
-
-document.querySelector('.btn-roll').addEventListener('click', result)
-
-                           //fonction hold
+                          //function hold
 
 function hold () {
-if(gaming = true) {
 
-scoreTotal += score;
-document.querySelector('.score' + player).textContent = scoreTotal;
-console.log(scoreTotal)
-nextPlayer(); 
+if(activePlayer == player1){
 
+scoreTotal1.textContent = parseInt(scoreTotal1.textContent) + parseInt(score1.textContent);
+scoreTotal1.textContent >= 100 ? win(): nextPlayer ();
 }
-else {
-  gaming = false;
-}};
+else{
 
-
-document.querySelector('.btn-hold').addEventListener('click', hold)
-
-                          //function reset
-function reset () {
-
-  player = 1;
-  gaming = true;
-  score = 0;
-  scoreTotal = 0;
-
-  document.querySelector('.scoreCurrent1').textContent = '0';
-  document.querySelector('.scoreCurrent2').textContent = '0';
-  document.querySelector('.score1').textContent = '0';
-  document.querySelector('.score2').textContent = '0';
-  document.querySelector('.player1').classList.add('turn');
-  document.querySelector('.player2').classList.remove('turn');
+scoreTotal2.textContent = parseInt(scoreTotal2.textContent) + parseInt(score2.textContent);
+scoreTotal2.textContent >= 100 ? win(): nextPlayer ();
+}
 };
 
+                         //function win
+
+function win () {
+if(activePlayer == player1){
+  $('#myModal').appendTo("body").modal('show');
+
+}
+else{
+  $('#myModal2').appendTo("body").modal('show');
+}
+reset();
+};
+                        //function reset
+
+function reset () {
+
+score1.textContent = '0';
+score2.textContent = '0';
+scoreTotal1.textContent = '0';
+scoreTotal2.textContent = '0';
+document.querySelector('.player1').classList.add('turn');
+document.querySelector('.player2').classList.remove('turn');
+};
+
+
+                            //button
+document.querySelector('.btn-roll').addEventListener('click', roll)
 document.querySelector('.btn-game').addEventListener('click', reset)
+document.querySelector('.btn-hold').addEventListener('click', hold)
